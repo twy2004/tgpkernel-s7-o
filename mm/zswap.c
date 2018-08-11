8dcb23f48fbfef97f074a583500bdd8daae72daa
twy_2000 <twy2004@gmail.com>
2018-08-11 21:19:49 +0800

    zswap union

--- mm/zswap.c
+++ mm/zswap.c
@@ -230,8 +230,10 @@ struct zswap_entry {
 	pgoff_t offset;
 	int refcount;
 	unsigned int length;
-	unsigned long handle;
-	unsigned long value;
+	union{
+		unsigned long handle;
+		unsigned long value;
+	}
 };
 
 #ifdef CONFIG_ZSWAP_ENABLE_WRITEBACK
@@ -949,7 +951,6 @@ static int zswap_frontswap_store(unsigned type, pgoff_t offset,
 		entry->offset = offset;
 		entry->length = 0;
 		entry->value = value;
-		entry->handle = 0;
 		kunmap_atomic(src);
 		goto insert_entry;
 	}
